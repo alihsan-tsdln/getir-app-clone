9a760064f314bc8f2cfa989374cc7b869a3b6b99
alihsan.tsdln <alihsan.tsdln@gmail.com>
2023-03-21 00:17:21 +0300

    Added Account

---
+++ src/Prize.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {
+  View,
+  Text,
+} from "react-native";
+
+export default function Prize() {
+    return (
+        <View>
+            <Text>Hello World!</Text>
+        </View>
+    );
+}
